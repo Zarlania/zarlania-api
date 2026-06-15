ad2419084ea6917f4e3fe5ed70112fa9c42a84a5
Steven Timothy <steven.timothy.cp@gmail.com>
2026-06-14 22:09:24 -0600

    Add GET / hello endpoint with MockMvc test

---
+++ src/test/java/com/zarlania/api/HelloControllerTest.java
@@ -0,0 +1,24 @@
+package com.zarlania.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(HelloController.class)
+class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void rootReturnsHelloMessage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value("Hello from Zarlania API"));
+    }
+}
