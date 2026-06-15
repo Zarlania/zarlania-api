84f491469b676602d642c5e2ab0aad6a09d4508f
Steven Timothy <steven.timothy.cp@gmail.com>
2026-06-14 22:08:45 -0600

    Scaffold Spring Boot project (Java 25, Maven)

---
+++ src/main/java/com/zarlania/api/ZarlaniaApiApplication.java
@@ -0,0 +1,13 @@
+package com.zarlania.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZarlaniaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZarlaniaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zarlania/api/ZarlaniaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zarlania.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZarlaniaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
